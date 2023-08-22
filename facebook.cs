db2e56cba980885d1b79c652b9a5ad24d0e4ea26
Phil69691 <114498774+Phil69691@users.noreply.github.com>
2023-08-22 02:24:04 +0100

    Update facebook.cs

--- facebook.cs
+++ facebook.cs
@@ -1,74 +1,62 @@
 using System;
 using System.Collections.Generic;
 
-public class Post
+class Post
 {
-    protected string message;
-    protected List<string> comments;
-    protected int likes;
+    public string Message { get; }
+    public int Likes { get; private set; }
+    public List<string> Comments { get; }
 
     public Post(string message)
     {
-        this.message = message;
-        this.comments = new List<string>();
-        this.likes = 0;
-    }
-
-    public void AddComment(string comment)
-    {
-        this.comments.Add(comment);
+        Message = message;
+        Likes = 0;
+        Comments = new List<string>();
     }
 
     public void AddLike()
     {
-        this.likes++;
+        Likes++;
     }
 
-    public virtual void Display()
+    public void AddComment(string comment)
+    {
+        Comments.Add(comment);
+    }
+
+    public void Display()
     {
         Console.WriteLine($"--------------------------");
-        Console.WriteLine($"Post: {message}");
-        Console.WriteLine($"Likes: {likes}");
-        Console.WriteLine($"Comments ({comments.Count}):");
-        foreach (string comment in comments)
+        Console.WriteLine($"Post: {Message}");
+        Console.WriteLine($"Likes: {Likes}");
+        Console.WriteLine($"Comments ({Comments.Count}):");
+        for (int i = 0; i < Comments.Count; i++)
         {
-            Console.WriteLine($"- {comment}");
+            Console.WriteLine($"{i + 1}. {Comments[i]}");
         }
         Console.WriteLine($"--------------------------");
     }
 }
 
-public class ImagePost : Post
+class Program
 {
-    protected string imageFileName;
-
-    public ImagePost(string message, string imageFileName) : base(message)
-    {
-        this.imageFileName = imageFileName;
-    }
-
-    public override void Display()
-    {
-        base.Display();
-        Console.WriteLine($"Image: {imageFileName}");
-    }
-}
-
-public class Program
-{
-    static void Main(string[] args)
+    static void Main()
     {
         List<Post> posts = new List<Post>();
 
         while (true)
         {
-            Console.WriteLine($"-------- Social Media App --------");
-            Console.WriteLine($"1. Add a text post");
-            Console.WriteLine($"2. Add an image post");
-            Console.WriteLine($"3. View all posts");
-            Console.WriteLine($"4. Exit");
+            Console.WriteLine("-------- Social Media App --------");
+            Console.WriteLine("1. Add a post");
+            Console.WriteLine("2. View all posts");
+            Console.WriteLine("3. Exit");
             Console.Write("Enter your choice: ");
-            int choice = int.Parse(Console.ReadLine());
+
+            if (!int.TryParse(Console.ReadLine(), out int choice))
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+                continue;
+            }
 
             Console.Clear();
 
@@ -77,40 +65,41 @@ public class Program
                 case 1:
                     Console.Write("Enter your message: ");
                     string message = Console.ReadLine();
-                    Post messagePost = new Post(message);
-                    posts.Add(messagePost);
-                    Console.WriteLine($"Post added successfully!");
+                    Post post = new Post(message);
+
+                    // Add comments to the post
+                    while (true)
+                    {
+                        Console.Write("Add a comment (or press Enter to finish): ");
+                        string comment = Console.ReadLine();
+                        if (string.IsNullOrWhiteSpace(comment))
+                            break;
+                        post.AddComment(comment);
+                    }
+
+                    posts.Add(post);
+                    Console.WriteLine("Post added successfully!");
                     break;
 
                 case 2:
-                    Console.Write("Enter your message: ");
-                    string imageMessage = Console.ReadLine();
-                    Console.Write("Enter image file name: ");
-                    string imageFileName = Console.ReadLine();
-                    ImagePost imagePost = new ImagePost(imageMessage, imageFileName);
-                    posts.Add(imagePost);
-                    Console.WriteLine($"Post added successfully!");
+                    Console.WriteLine("-------- All Posts --------");
+                    foreach (Post p in posts)
+                    {
+                        p.Display();
+                    }
+                    Console.WriteLine("--------------------------");
                     break;
 
                 case 3:
-                    Console.WriteLine($"-------- All Posts --------");
-                    foreach (Post post in posts)
-                    {
-                        post.Display();
-                    }
-                    Console.WriteLine($"--------------------------");
-                    break;
-
-                case 4:
-                    Console.WriteLine($"Exiting program...");
+                    Console.WriteLine("Exiting program...");
                     return;
 
                 default:
-                    Console.WriteLine($"Invalid choice! Please try again.");
+                    Console.WriteLine("Invalid choice! Please try again.");
                     break;
             }
 
-            Console.WriteLine($"Press any key to continue...");
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
         }
